14611cadfa32d55496199b9bd7e91aa7e0ad6533
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-07-11 15:22:25 +0530

    commit-3

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ const http=require("http")
 const bodyParser=require("body-parser")
 const path=require("path")
 const app=express()
+const adminRouter=require("./routes/admin")
+const shopRouter=require("./routes/shop")
 const server=http.createServer(app)
 app.use(bodyParser({extended:true}))
 
+app.use(adminRouter)
+app.use(shopRouter)
 
-app.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,"index.html"))
-})
-app.post("/",(req,res,next)=>{
-    const name=req.body.name
-    const size=req.body.size
-    console.log(name)
-    console.log(size)
-})
 
 
 server.listen(3000,()=>console.log("Server started on port 3000"))
